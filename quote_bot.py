9624c7bcaf75947f70be8dedd5c22511e4651892
jumblesale <rob@robsollars.co.uk>
2014-12-09 13:13:27 +0000

    Removed comments

--- quote_bot.py
+++ quote_bot.py
@@ -20,16 +20,16 @@ parser.add_option("-n", "--nick", dest="nick", default='quote_bot',
 (options, args) = parser.parse_args()
 
 
-def ping(): # This is our first function! It will respond to server Pings.
+def ping():
   ircsock.send("PONG :pingis\n")  
 
-def sendmsg(chan , msg): # This is the send message function, it simply sends messages to the channel.
+def sendmsg(chan , msg):
   ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n") 
 
-def joinchan(chan): # This function is used to join channels.
+def joinchan(chan):
   ircsock.send("JOIN "+ chan +"\n")
 
-def hello(): # This function responds to a user that inputs "Hello Mybot"
+def hello():
   ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
 
 def random_quote(channel):
@@ -37,32 +37,31 @@ def random_quote(channel):
   ircsock.send("PRIVMSG "+ channel +" :" + quote + "\n")
 
 def connect(server, channel, botnick):
-  ircsock.connect((server, 6667)) # Here we connect to the server using the port 6667
+  ircsock.connect((server, 6667))
   ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This bot is a result of a tutoral covered on http://shellium.org/wiki.\n") # user authentication
-  ircsock.send("NICK "+ botnick +"\n") # here we actually assign the nick to the bot
+  ircsock.send("NICK "+ botnick +"\n")
 
-  joinchan(channel) # Join the channel using the functions we previously defined
+  joinchan(channel)
 
 def mentions(msg):
   print msg
 
   
 def listen():
-  while 1: # Be careful with these! it might send you to an infinite loop
-    
+  while 1:
 
-    ircmsg = ircsock.recv(2048) # receive data from the server
-    ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
+    ircmsg = ircsock.recv(2048)
+    ircmsg = ircmsg.strip('\n\r')
     
     print ircmsg
 
-    if ircmsg.find(":!quote") != -1: # If someone says !quote
+    if ircmsg.find(":!quote") != -1:
       random_quote()
 
     if ircmsg.find(":!mentions") != -1:
         mentions(ircmsg)
 
-    if ircmsg.find("PING :") != -1: # if the server pings us then we've got to respond!
+    if ircmsg.find("PING :") != -1:
       ping()
 
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
