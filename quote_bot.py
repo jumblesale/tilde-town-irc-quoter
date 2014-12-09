d816108b7a99fa78d8abe5ff93e6d2b67a4add54
jumblesale <rob@robsollars.co.uk>
2014-12-09 12:56:15 +0000

    Added method to get system users

--- quote_bot.py
+++ quote_bot.py
@@ -5,8 +5,10 @@
 import socket
 import os
 
+import get_users
+
 # Some basic variables used to configure the bot        
-server = "127.0.0.1" # Server
+server  = "127.0.0.1" # Server
 channel = "#tildetown" # Channel
 botnick = "quote_bot" # Your bots nick
 
@@ -27,19 +29,25 @@ def random_quote():
   quote = os.popen("/home/frs/quotes/randquote.py").read()
   ircsock.send("PRIVMSG "+ channel +" :" + quote + "\n")
 
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ircsock.connect((server, 6667)) # Here we connect to the server using the port 6667
-ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This bot is a result of a tutoral covered on http://shellium.org/wiki.\n") # user authentication
-ircsock.send("NICK "+ botnick +"\n") # here we actually assign the nick to the bot
+def connect():
+  ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  ircsock.connect((server, 6667)) # Here we connect to the server using the port 6667
+  ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This bot is a result of a tutoral covered on http://shellium.org/wiki.\n") # user authentication
+  ircsock.send("NICK "+ botnick +"\n") # here we actually assign the nick to the bot
 
-joinchan(channel) # Join the channel using the functions we previously defined
+  joinchan(channel) # Join the channel using the functions we previously defined
 
-while 1: # Be careful with these! it might send you to an infinite loop
-  ircmsg = ircsock.recv(2048) # receive data from the server
-  ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
+  
+def listen():
+  while 1: # Be careful with these! it might send you to an infinite loop
+    
 
-  if ircmsg.find(":!quote") != -1: # If someone says !quote
-    random_quote()
+    ircmsg = ircsock.recv(2048) # receive data from the server
+    ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
+    print ircmsg
 
-  if ircmsg.find("PING :") != -1: # if the server pings us then we've got to respond!
-    ping()
+    if ircmsg.find(":!quote") != -1: # If someone says !quote
+      random_quote()
+
+    if ircmsg.find("PING :") != -1: # if the server pings us then we've got to respond!
+      ping()
